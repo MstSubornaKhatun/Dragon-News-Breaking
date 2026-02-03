2affbdaa556182902764f293b486d8f854cc6ce4
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-03 16:24:14 +0600

    marquee

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header/>
+                 <section w="11/12 mx-auto">
+                <LatestNews></LatestNews>
+            </section>
+            <nav>
+                <Navbar/>
+            </nav>
             </header>
+           
             <main>
                 <section className='left_nav'></section>
                 <section className='main'>
